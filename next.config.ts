688cd98d40e04b5f95651467659a1e32157192e2
samiyantannaz1 <samiyantannaz1@gmail.com>
2026-08-24 14:11:23 +0330

    Configure static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
